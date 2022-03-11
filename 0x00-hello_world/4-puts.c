db7ba638738e89416fe37e970d3592423c6b47fa
Alabi Khalid <81325840+DeanSft-dev@users.noreply.github.com>
2022-03-11 01:04:22 +0100

    a C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
  * main - Entry point
  * 
  * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle"
+ * this program prints "Programming is like building a multilingual puzzle
  * Return:0
  */
  int main()
 {
- 	puts("Programming is like building a multilingual puzzle \n");
+ 	puts("Programming is like building a multilingual puzzle \n);
 	return (0);
 }
