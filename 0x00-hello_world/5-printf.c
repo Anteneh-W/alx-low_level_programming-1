728efe961b4ae14ad7884287e620a0529967f706
Alabi Khalid <81325840+DeanSft-dev@users.noreply.github.com>
2022-03-11 01:31:34 +0100

    C program to print using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: using the main function
- * this program prints "with proper grammar, 
-   but the outcome is a piece of art,\n"
  * Return:0
  */
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
