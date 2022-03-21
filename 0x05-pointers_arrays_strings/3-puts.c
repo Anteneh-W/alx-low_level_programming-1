3e8848209b2793caad5c5e5487bbd5521e47481c
Alabi Khalid <81325840+DeanSft-dev@users.noreply.github.com>
2022-03-21 21:29:34 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <string.h>
+
 /**
- * 3_puts - prints a string followed by new line to stdout
+ * _puts - prints a string followed by new line to stdout
  * @str: string
  * Return: 0
  */
 
-void 3_puts(char *str)
+void _puts(char *str)
 {
 	puts(str);
 }
