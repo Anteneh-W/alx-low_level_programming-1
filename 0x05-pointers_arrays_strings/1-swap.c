ec9d6f9a4d82454854cfa682cb83a83561bb04aa
Alabi Khalid <alabikhalid.12@gmail.com>
2022-03-21 12:00:30 -0700

    1-swap.c created

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - takes two integer values and swap them
+ * @a: pointer to first value
+ * @a: pointer to second value
+ */ 
+void swap_int(int *a, int *b)
+{
+	c = *a;
+	*a = *b;
+	*b = c;
+}
