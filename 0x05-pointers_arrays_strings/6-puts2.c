5811b6c009396fb02935da8acd9e3beaa1b37e50
Alabi Khalid <81325840+DeanSft-dev@users.noreply.github.com>
2022-03-22 15:27:07 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** puts2 - print other charcter of string starting from first charcter
+/** 
+ * puts2 - print other charcter of string starting from first charcter
  * @str: string
  */
 
